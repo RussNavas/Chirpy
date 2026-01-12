ee3c186cf57184a916b54e9ca06edc3a66159183
RussNavas <navasr@oregonstate.edu>
2026-01-12 14:30:47 -0800

    added new metric functionality and method specific routing

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
-import(
-	"net/http"
+import (
 	"log"
+	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct{
+		fileserverHits atomic.Int32
+	}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	fileServerHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServerHandler))
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:	":" + port,
@@ -21,3 +34,17 @@ func main() {
 	log.Fatal(srv.ListenAndServe())
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, req *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %v", hits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request){
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
