910b75e33772e0b51c2103d94deaed596556892f
RussNavas <navasr@oregonstate.edu>
2026-01-18 00:22:04 -0800

    using ignore

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,8 +1,11 @@
 package auth
 
-import(
+import (
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
+
 	"github.com/alexedwards/argon2id"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
@@ -62,4 +65,19 @@ func ValidateJWT(tokenString, tokenSecret string)(uuid.UUID, error){
 	}
 
 	return uuID, nil
+}
+
+
+func GetBearerToken(headers http.Header) (string, error){
+	authHeader := headers.Get("Authorization")
+	if authHeader == ""{
+		return "", fmt.Errorf("no authorization header included")
+	}
+
+	splitAuth := strings.Split(authHeader, " ")
+
+	if len(splitAuth) < 2 || splitAuth[0] != "Bearer"{
+		return "", fmt.Errorf("malformed authorization header")
+	}
+	return splitAuth[1], nil
 }
